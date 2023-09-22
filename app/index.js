98c8c74057a562583a0ce83c79a5b9760e80a06c
Louis Succio <louis.succio@yourbind.com>
2023-09-22 10:44:44 -0500

    also add a link for posterity

--- app/index.js
+++ app/index.js
@@ -1,10 +1,11 @@
-import { router } from "expo-router";
+import { Link, router } from "expo-router";
 import { Text, View } from "react-native-web";
 import { Pressable } from "react-native";
 
 export default function MainIndex() {
   return <View>
     <Pressable onPress={() => router.push("/dynamic")}><Text style={{ color: "blue" }}>Dynamic Base Path</Text></Pressable>
-    <Pressable onPress={() => router.push("/dynamic/123/details")}><Text style={{ color: "blue" }}>Dynamic Details 123</Text></Pressable>
+    <Pressable onPress={() => router.push("/dynamic/123/details")}><Text style={{ color: "blue" }}>Dynamic Details 123 with router.push</Text></Pressable>
+    <Link href="/dynamic/123/details"><Text style={{ color: "blue" }}>Dynamic Details 123 with Link</Text></Link>
   </View>
 }
